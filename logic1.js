28eefa16b5395603ab8503e60b026dadfa315bb7
Christopher Faison <pmatrixmedia@gmail.com>
2018-03-12 23:53:51 -0500

    Version 2.0

--- logic1.js
+++ logic1.js
@@ -15,7 +15,7 @@ function buildQueryURL() {
 }
 
 
-   
+
 
 // CLICK HANDLERS
 // ==========================================================
@@ -37,37 +37,50 @@ $("#submit").on("click", function () {
 
         // After the data comes back from the API
     }).then(function (response) {
-            // Storing an array of results in the results variable
-            var results = response.data;
+        // Storing an array of results in the results variable
+        var results = response.data;
 
-            // Looping over every result item
-            for (var i = 0; i < results.length; i++) {
-                // Only taking action if the photo has an appropriate rating
-                
+        // Looping over every result item
+        for (var i = 0; i < results.length; i++) {
+            // Only taking action if the photo has an appropriate rating
 
-                    // Creating a div with the class "item"
-                    var gifDiv = $("<div class='item'>");
 
-                
+            // Creating a div with the class "item"
+            var gifDiv = $("<div class='item'>");
 
-                
 
-                    // Creating an image tag
-                    var searchCriteria = $("<img>");
 
-                    // Giving the image tag an src attribute of a proprty pulled off the
-                    // result item
+            // Creating an image tag
+            var searchCriteria = $("<img>");
 
-                    searchCriteria.attr("src",
-                        results[i].images.fixed_height.url);
+            // Giving the image tag an src attribute of a proprty pulled off the
+            // result item
 
-                    gifDiv.append();
-                    gifDiv.append(searchCriteria);
+            searchCriteria.attr("src",
+                results[i].images.fixed_height.url);
 
-                    // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
-                    $("#gifs-appear-here").prepend(gifDiv);
-            }
-            
-        })
+            gifDiv.append();
+            gifDiv.append(searchCriteria);
 
-     });
+            // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
+            $("#gifs-appear-here").prepend(gifDiv);
+        }
+
+    });
+
+});
+
+$("src").on("click", function () {
+    // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+    var state = $(this).attr("data-state");
+    // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+    // Then, set the image's data-state to animate
+    // Else set src to the data-still value
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+    } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+    }
+});
